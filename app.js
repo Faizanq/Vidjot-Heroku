6158a7534d4e32a59242c38a0aa76c170697b6db
faizanq <faizanqureshi0@gmail.com>
2018-09-18 07:50:50 +0530

    simple route setup with handlebars

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+
+const port = process.env.PORT || 3002;
+
+//our engine layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//middleware
+app.use(function(req,res,next){
+	console.log(Date.now());
+	next();
+});
+
+//Index route
+app.get('/',function(req,res){
+	res.render('home');
+});
+
+//About route
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
+
+app.listen(port,()=>{
+	console.log(`Server start listening on port ${port}`);
+});
