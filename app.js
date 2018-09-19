408367e8cdb3bd8467acf61f143460d8a76e7d0d
faizanq <faizanqureshi0@gmail.com>
2018-09-19 09:05:30 +0530

    install and require mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3002;
 
